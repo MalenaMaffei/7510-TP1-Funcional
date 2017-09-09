c1d06c2ca4a022760841e68aeb15ffa1bb0dc236
MalenaMaffei <malenamaffei@hotmail.com>
2017-09-09 19:44:37 -0300

    refactor in validator

--- src/validator.clj
+++ src/validator.clj
@@ -1,9 +1,22 @@
 (ns validator)
 
+(def fact-syntax "[a-z\\-]+\\([a-z\\-]+(, [a-z\\-]+){0,}\\)")
+
+
+
 (defn is-valid-fact?
     "checks if input has valid fact format"
     [fact]
-    (if (re-find #"^[a-z\-]+\([a-z\-]+(, [a-z\-]+){0,}\)$" fact)
+    (if (re-find (re-pattern (str "^" fact-syntax "$")) fact)
+        true
+        false
+    )
+)
+
+(defn is-valid-database?
+    "checks if input has valid fact format"
+    [database]
+    (if (re-find (re-pattern (str "^" fact-syntax "\\.$")) database)
         true
         false
     )
--- test/invalid_queries_test.clj
+++ test/invalid_queries_test.clj
@@ -33,5 +33,48 @@
             false)
         )
     )
+
+    (testing "varon(juan, pepe ) should be false"
+        (is (= (is-valid-fact? "varon(juan, pepe )")
+            false)
+        )
+    )
 )
 
+(deftest invalid-database-test
+    (testing "varon(juan) should be true"
+        (is (= (is-valid-fact? "varon(juan)")
+            true)
+        )
+    )
+
+    (testing "varon(juan) should be false"
+        (is (= (is-valid-fact? "varon(juan )")
+            false)
+        )
+    )
+    
+    (testing "varon(juan, ) should be false"
+        (is (= (is-valid-fact? "varon(juan, )")
+            false)
+        )
+    )
+
+    (testing "amigos(juan, pepe, adrian, elias) should be true"
+        (is (= (is-valid-fact? "amigos(juan, pepe, adrian, elias)")
+            true)
+        )
+    )
+
+    (testing "varon(juan,pepe) should be false"
+        (is (= (is-valid-fact? "varon(juan,pepe)")
+            false)
+        )
+    )
+
+    (testing "varon(juan, pepe ) should be false"
+        (is (= (is-valid-fact? "varon(juan, pepe )")
+            false)
+        )
+    )
+)
