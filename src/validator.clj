8b1c71d6887cba90aeae466874951d561fd35c48
MalenaMaffei <malenamaffei@hotmail.com>
2017-09-10 12:48:18 -0300

    started validating db

--- src/validator.clj
+++ src/validator.clj
@@ -1,32 +1,44 @@
-(ns validator)
+(ns validator
+    (:require [clojure.string :refer :all])    
+)
 
 (def fact-syntax "[a-z\\-]+\\([a-z\\-]+(, [a-z\\-]+){0,}\\)")
 
 (def rule-syntax (str fact-syntax " :\\- " fact-syntax "(, " fact-syntax "){0,}"))
 
-(defn is-valid-fact?
+(defn delimit-pattern
+    "delimits a regex pattern"
+    [pattern]
+    (str "^" pattern "$")    
+)
+
+(defn valid-fact?
     "checks if input has valid fact format"
     [fact]
-    (if (re-find (re-pattern (str "^" fact-syntax "$")) fact)
+    (if (re-find (re-pattern (delimit-pattern fact-syntax)) fact)
         true
         false
     )
 )
 
-(defn is-valid-rule?
+(defn valid-rule?
     "checks if input has valid rule format"
     [rule]
-    (if (re-find (re-pattern (str "^" rule-syntax "$")) rule)
+    (if (re-find (re-pattern (delimit-pattern rule-syntax)) rule)
         true
         false
     )
 )
 
-(defn is-valid-database?
+(defn valid-line?
+    "checks if line is valid rule or fact"
+    [line]
+    (or (valid-rule? line) (valid-fact? line))
+)
+
+(defn valid-database?
     "checks if input has valid fact format"
     [database]
-    (if (re-find (re-pattern (str "^" fact-syntax "\\.$")) database)
-        true
-        false
-    )
+    (map valid-line? (split-lines database))
 )
+
--- test/invalid_queries_test.clj
+++ test/invalid_queries_test.clj
@@ -3,39 +3,48 @@
             [validator :refer :all]))
 
 
+(def incomplete-database "
+	varon(juan).
+    varon.
+    padre(juan,pepe).
+    hola(que, tal).
+    saludar :- hola(juan), hola(pepe).
+")
+           
+
 (deftest facts-syntax-test
     (testing "varon(juan) should be true"
-        (is (= (is-valid-fact? "varon(juan)")
+        (is (= (valid-fact? "varon(juan)")
             true)
         )
     )
 
     (testing "varon(juan) should be false"
-        (is (= (is-valid-fact? "varon(juan )")
+        (is (= (valid-fact? "varon(juan )")
             false)
         )
     )
     
     (testing "varon(juan, ) should be false"
-        (is (= (is-valid-fact? "varon(juan, )")
+        (is (= (valid-fact? "varon(juan, )")
             false)
         )
     )
 
     (testing "amigos(juan, pepe, adrian, elias) should be true"
-        (is (= (is-valid-fact? "amigos(juan, pepe, adrian, elias)")
+        (is (= (valid-fact? "amigos(juan, pepe, adrian, elias)")
             true)
         )
     )
 
     (testing "varon(juan,pepe) should be false"
-        (is (= (is-valid-fact? "varon(juan,pepe)")
+        (is (= (valid-fact? "varon(juan,pepe)")
             false)
         )
     )
 
     (testing "varon(juan, pepe ) should be false"
-        (is (= (is-valid-fact? "varon(juan, pepe )")
+        (is (= (valid-fact? "varon(juan, pepe )")
             false)
         )
     )
@@ -43,31 +52,31 @@
 
 (deftest rules-syntax-test
     (testing "varon(juan) should be false"
-        (is (= (is-valid-rule? "varon(juan)")
+        (is (= (valid-rule? "varon(juan)")
             false)
         )
     )
 
     (testing "varon(juan) :-  should be false"
-        (is (= (is-valid-rule? "varon(juan) :- ")
+        (is (= (valid-rule? "varon(juan) :- ")
             false)
         )
     )
 
     (testing "varon(juan) :- hola(juan) should be true"
-        (is (= (is-valid-rule? "varon(juan) :- hola(juan)")
+        (is (= (valid-rule? "varon(juan) :- hola(juan)")
             true)
         )
     )
 
     (testing "varon(juan):-hola(juan) should be false"
-        (is (= (is-valid-rule? "varon(juan):-hola(juan)")
+        (is (= (valid-rule? "varon(juan):-hola(juan)")
             false)
         )
     )
 
     (testing "saludar(pepe, carlos) :- hola(pepe), hola(carlos) should be true"
-        (is (= (is-valid-rule? "saludar(pepe, carlos) :- hola(pepe), hola(carlos)")
+        (is (= (valid-rule? "saludar(pepe, carlos) :- hola(pepe), hola(carlos)")
             true)
         )
     )
