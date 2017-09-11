c6c00b3d3b2e1983f60d4bdbf05dd647f550ae2d
MalenaMaffei <malenamaffei@hotmail.com>
2017-09-11 19:25:29 -0300

    refactor + docs

--- src/parser.clj
+++ src/parser.clj
@@ -15,12 +15,6 @@
   (.contains database fact)
 )
 
-(defn get-params
-  "gets parameters from fact or rule"
-  [query]
-  (split (second (re-matches #"^[a-z]+\(([^\)]+)\).*$" query)) param-delimiter)
-)
-
 (defn fill-rule
   "replaces all variables from rule-definition with the corresponding parametrs from the query"
   [rule-definition rule-params query-params]
@@ -29,12 +23,7 @@
   (apply str (clojure.string/replace rule-definition (re-pattern to-replace) replacement-map))
 )
 
-(defn get-rule-facts
-  "gets list of facts that compose given rule"
-  [rule-definition rule-params query-params]
-  (def filled-rule (fill-rule rule-definition rule-params query-params))
-  (split (clojure.string/replace (second (re-matches #"^.+ :- ([a-z]+\(.+\))$" filled-rule)) #"(\)), " "$1.") db-delimiter)
-)
+
 
 (defn get-rules
   "filters all rules from the db"
--- src/syntax_validator.clj
+++ src/syntax_validator.clj
@@ -8,14 +8,20 @@
 (def fact-syntax #"^[a-z\-]+\([a-z\-]+(, [a-z\-]+){0,}\)$")
 (def rule-syntax #"^[a-z\-]+\([A-Z](, [A-Z]+){0,}\) :\- [a-z\-]+\([A-Z](, [A-Z]+){0,}\)(, [a-z\-]+\([A-Z](, [A-Z]+){0,}\)){0,}$")
 
-
-; (defn gen-pattern
-;     "generates regex pattern"
-;     [pattern]
-;     (re-pattern (str "^" pattern "$"))
-; )
-
 ;"TODO: rules tienen que ser validadas un poco mas, que las variables coincidan por lo menos
+
+(defn get-params
+  "gets parameters from fact or rule"
+  [query]
+  (split (second (re-matches #"^[a-z]+\(([^\)]+)\).*$" query)) param-delimiter)
+)
+
+(defn get-rule-facts
+  "gets list of facts that compose given rule"
+  [rule-definition rule-params query-params]
+  (def filled-rule (fill-rule rule-definition rule-params query-params))
+  (split (clojure.string/replace (second (re-matches #"^.+ :- ([a-z]+\(.+\))$" filled-rule)) #"(\)), " "$1.") db-delimiter)
+)
 
 (defn valid-fact?
     "checks if input has valid fact format"
