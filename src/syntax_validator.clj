b2f888edd2d2a212d825838b1e97bf5f24ad5638
MalenaMaffei <malenamaffei@hotmail.com>
2017-09-11 19:52:38 -0300

    refactor + docs

--- src/syntax_validator.clj
+++ src/syntax_validator.clj
@@ -10,19 +10,6 @@
 
 ;"TODO: rules tienen que ser validadas un poco mas, que las variables coincidan por lo menos
 
-(defn get-params
-  "gets parameters from fact or rule"
-  [query]
-  (split (second (re-matches #"^[a-z]+\(([^\)]+)\).*$" query)) param-delimiter)
-)
-
-(defn get-rule-facts
-  "gets list of facts that compose given rule"
-  [rule-definition rule-params query-params]
-  (def filled-rule (fill-rule rule-definition rule-params query-params))
-  (split (clojure.string/replace (second (re-matches #"^.+ :- ([a-z]+\(.+\))$" filled-rule)) #"(\)), " "$1.") db-delimiter)
-)
-
 (defn valid-fact?
     "checks if input has valid fact format"
     [fact]
